e8b7972666d8758ee768414e5c3e4ad4625b4922
Kevin Sundqvist Norlén <kevin.sundqvistnorlen@gmail.com>
2024-08-09 04:57:05 +0200

    Patch

--- bin/lzmasz.js
+++ bin/lzmasz.js
@@ -3,24 +3,25 @@
 import fs from "node:fs/promises";
 import { PassThrough } from "node:stream";
 
+import chalk from "chalk";
 import Gitignore from "gitignore-fs";
+import lzma from "lzma-native";
+import prettyBytes from "pretty-bytes";
+
 const gitignore = new Gitignore();
 
-import lzma from "lzma-native";
 const compressor = lzma.createCompressor({
     check: lzma.CHECK_NONE,
     threads: 8,
     preset: 9,
 });
 
-import prettyBytes from "pretty-bytes";
-import chalk from "chalk";
-
 async function* walk(dir) {
     for await (const dirent of await fs.opendir(dir)) {
-        if (await gitignore.ignores(dirent.path)) continue;
-        if (dirent.isDirectory()) yield* walk(dirent.path);
-        else if (dirent.isFile()) yield dirent.path;
+        const file = `${dir}/${dirent.name}`;
+        if (await gitignore.ignores(file)) continue;
+        if (dirent.isDirectory()) yield* walk(file);
+        else if (dirent.isFile()) yield file;
     }
 }
 
@@ -38,17 +39,20 @@ const output = new PassThrough();
 input.pipe(compressor).pipe(output);
 
 (async () => {
+    let total = 0;
     for await (const file of walk(".")) {
         const buffer = await fs.readFile(file);
 
         if (isValidUTF8(buffer)) {
-            console.debug(chalk.gray(`+ ${file}`));
+            console.debug(chalk.blue(`+ ${file}`));
             const padded = Buffer.concat([buffer, Buffer.alloc(1)], buffer.length + 1);
             await new Promise((resolve) => input.write(padded, resolve));
+            total += buffer.length;
         }
     }
 
     input.end();
+    console.debug(chalk.blue(`\n${prettyBytes(total)} (${total.toLocaleString()} bytes)`));
 })();
 
 let size = 0;
@@ -56,4 +60,4 @@ for await (const chunk of output) {
     size += chunk.length;
 }
 
-console.log(chalk.bold(`${prettyBytes(size)} (${size.toLocaleString()} bytes)`));
+console.log(chalk.bold(`${prettyBytes(size)} (${size.toLocaleString()} bytes) compressed`));
